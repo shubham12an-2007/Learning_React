fa40bc4a3300da06f5cb7f6574edbc2cc56b49f7
Shubham Kumar Singh <shubham12an@gmail.com>
2026-03-02 13:50:23 +0530

    learnt a new concept of react - hooks (useState)

---
+++ counter_project/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  let [counter, setCounter] = useState(5);
+
+  const addValue = () => {
+    setCounter((counter = counter + 1));
+
+    if (counter > 20) {
+      setCounter((counter = 20));
+    }
+    // console.log(counter);
+  };
+
+  const removeValue = () => {
+    setCounter((counter = counter - 1));
+
+    if (counter < 0) {
+      setCounter((counter = 0));
+    }
+    // console.log(counter);
+  };
+
+  // make a logic so that counter  never goes in negative
+
+  return (
+    <>
+      <h1>Chai aur React </h1>
+      <h2>Counter Value: {counter} </h2>
+
+      <button onClick={addValue}>Add Value {counter} </button>
+      <br />
+      <button onClick={removeValue}>Remove Value {counter} </button>
+
+      <p>Footer : {counter} </p>
+    </>
+  );
+}
+
+export default App;
