cb5b78145869074dbbea8b4f9f50b501c582999f
Shubham Kumar Singh <shubham12an@gmail.com>
2026-03-07 12:45:14 +0530

    made a color picker using  react

---
+++ color-picker/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import ColorPicker from "./ColorPicker";
+
+function App() {
+  return (
+    <>
+      <ColorPicker></ColorPicker>
+    </>
+  );
+}
+
+export default App;
